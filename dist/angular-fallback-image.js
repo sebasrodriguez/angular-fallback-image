95a86b6d678f7f80e1c92a6318ec5d2eba3bdce6
Sebastián Rodríguez <sebasrodriguez@gmail.com>
2016-02-12 11:51:15 -0300

    Updated distributable files and bower/npm packages

--- dist/angular-fallback-image.js
+++ dist/angular-fallback-image.js
@@ -5,10 +5,16 @@
 			link: function($scope, element, attributes) {				
 				var elem = element;
 				var attrs = attributes;
+				var width, height;
 				
 				element.on('error', function() {
 					if(elem.attr('src') === attrs.srFallback) {
 						throw new Error('The supplied fallback image doesn\'t exist');
+					}
+					if(!attrs.srFallback) {
+						width = elem[0].offsetWidth;
+						height = elem[0].offsetHeight;
+						attrs.srFallback = 'http://placehold.it/' + width + 'x' + height;
 					}
 					elem.attr('src', attrs.srFallback);
 				});
